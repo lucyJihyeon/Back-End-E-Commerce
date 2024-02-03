82afe155f5c9ed0b7102e311e7e6b46afcf68ad5
Lucy Kwon <ks02ks222@gmail.com>
2024-02-03 15:00:53 -0800

    set up associations between the models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  //if category field is deleted, delete the product as well.
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: 'product_tag'
+  },
+  as: 'product'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: 'product_tag'
+  },
+  as: 'tag'
+});
+
 
 module.exports = {
   Product,
